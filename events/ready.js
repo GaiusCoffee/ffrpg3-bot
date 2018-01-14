9244c066ea58a3a61681281d515113bf8247df7b
GaiusCoffee <gaius.coffee@gmail.com>
2018-01-14 22:32:40 +0800

    Fix ready event: Doesn't update settings guild list.

--- events/ready.js
+++ events/ready.js
@@ -2,7 +2,7 @@ module.exports = async client => {
 	await client.wait(1000);
 	client.logger.log(`[READY] ${client.user.tag}, ready to serve ${client.users.size} users in ${client.guilds.size} servers.`, "ready");
 	await client.guilds.forEach(async g => {
-		if (!(await client.settings.get("guilds").find({ id:g.id }))) {
+		if (!(await client.settings.get("guilds").find({ id:g.id }).value())) {
 			await client.settings.get("guilds").push({ id:g.id, settings:client.config.defaultSettings }).write();
 		}
 	});
