64902baf51ccdc2fff1ada4079201d91bbf417c8
GaiusCoffee <gaius.coffee@gmail.com>
2018-01-15 14:23:47 +0800

    Implement world.config.set

---
+++ commands/world/config/set.js
@@ -0,0 +1,44 @@
+const Discord = require("discord.js");
+exports.subcommands = ["s","set"];
+exports.run = async (client, message, args, level, worldname) => {
+    // Parameters
+    if (args.length <= 3) {
+		message.channel.send(
+			"🛑🛑 **ERROR** Kupopo!? **ERROR** 🛑🛑\n" + 
+			"Too few parameters for world.config.set command.");
+        return;
+    }
+    let guildId = message.guild.id,
+        configKey = args[1],
+        configValue = args[2],
+        configOldValue = "";
+    // Process
+    await client.db[guildId].get("worlds").find({ worldname:worldname }).value().config.forEach((element,index) => {
+        if (configKey === element.key) {
+            configOldValue = element.value;
+        }
+    });
+    await config.db[guildId].get("worlds").find({ worldname:worldname, key:configKey }).assign({ value:configValue }).write();
+    // Respond
+    if (configOldValue === "") {
+        message.channel.send(
+`= ${ worldname } = World Configuration =
+
+Configuration Key/Value pair not found.`
+            ,{code: "asciidoc"});
+    } else {
+        await client.db[guildId].get("worlds").find({ worldname:worldname }).value().config.forEach((element,index) => {
+            if (configKey === element.key) {
+                configElement = element;
+            }
+        });
+        message.channel.send(
+`= ${ worldname } = World Configuration =
+
+Config Key   : ${ configElement.key }
+Config Value : ${ configElement.value } (previous: ${ configOldValue })
+Value Default: ${ configElement.default }
+Description  : ${ configElement.description }`
+            ,{code: "asciidoc"});
+    }
+}
