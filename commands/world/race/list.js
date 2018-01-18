bc7de6cb5a5997c938599df8c699ab0e149e31d7
GaiusCoffee <gaius.coffee@gmail.com>
2018-01-18 14:33:08 +0800

    Implement world.race.list

--- commands/world/race/list.js
+++ commands/world/race/list.js
@@ -1,25 +1,27 @@
-const Discord = require("discord.js");
 exports.subcommands = ["l","ls","list"];
 exports.run = async (client, message, args, level, worldname) => {
     // Parameters
 	let guildId = message.guild.id,
-        raceList = [];
+        raceList = [], races = "";
     // Process
     // Get builtin
-
+    await client.gamedata.races.get("all").value().array.forEach((element, index) => {
+        raceList.push(element.race);
+    });
     // Get custom
-
     // Remove disabled
-
     // Build raceList
-    
     if (raceList.length === 0) {
-        raceList = "Apparently, this world has no sentient races, kupo!";
+        races = "Apparently, this world has no sentient races, kupo!";
+    } else {
+        raceList.forEach((element, index) => {
+            races += "• " + element;
+        });
     }
     // Respond
     message.channel.send(
 `= ${ worldname } = World Races =
 
-${ raceList }`
+${ races }`
         ,{code: "asciidoc"});
 }
