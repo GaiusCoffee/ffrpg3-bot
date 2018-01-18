928c15a956165f78a2bcf28cae37f9d9488e298f
GaiusCoffee <gaius.coffee@gmail.com>
2018-01-18 22:10:55 +0800

    Fix world.item.list

--- commands/world/item/list.js
+++ commands/world/item/list.js
@@ -3,92 +3,99 @@ exports.run = async (client, message, args, level, worldname) => {
     let guildId = message.guild.id,
         items = "",
         itemList = [];
-    if (args.length = 2) {
-        let itemName = args[1];
-        // Get builtin        
-        await client.gamedata.items.get("all").find({ name:itemName }).value().forEach((element, index) => {
-            itemList.push(element);
-        });
-        // Get custom
-        // Remove disabled
-        // Build items
-        if (itemList.length == 0) {
-            items = `Apparently, there are no items named ${ itemName } in this world, kupo...`;
-        } else {
-            items = `** ${ itemList[0].name } **\n\n` + 
-                `• Type        : ${ itemList[0].type }\n` +  
-                `• Slot        : ${ itemList[0].type }\n` +  
-                `• Tier        : ${ itemList[0].type }\n` +  
-                `• Cost        : ${ itemList[0].type }\n` +
-                `• Availability: ${ itemList[0].type }\n`;
-            if (itemName[0].weaponSkill) {
-                items += `• Weapon Skill: ${ itemList[0].weaponSkill }\n`;
+    if (args.length >= 2) {
+        if (!Number.isInteger(Number(args[1]))) {
+            args.shift();
+            let itemName = args.join(" ");
+            // Get builtin
+            itemList = await client.gamedata.items.get("all").find({ name:itemName }).value();
+            // Get custom
+            // Remove disabled
+            // Build items
+            if (!itemList) {
+                items = `Apparently, there are no items named ${ itemName } in this world, kupo...`;
+            } else {
+                items = 
+`**${ itemList.name }**
+
+• Type        : ${ itemList.type }
+• Slot        : ${ itemList.slot }
+• Tier        : ${ itemList.tier }
+• Cost        : ${ itemList.cost } gil
+• Availability: ${ itemList.avail }%\n`;
+                if (itemList.weaponSkill) {
+                    items += `• Weapon Skill: ${ itemList.weaponSkill }\n`;
+                }
+                if (itemList.dmgMulti) {
+                    items += `• Damage Code : (${ itemList.dmgMulti } x ${ itemList.dmgAttr.toUpperCase() }) + ${ itemList.dmgDice }\n`;
+                }
+                if (itemList.armBonus) {
+                    items += `• ARM Bonus   : +${ itemList.armBonus }\n`
+                }
+                if (itemList.marmBonus) {
+                    items += `• MARM Bonus  : +${ itemList.marmBonus }\n`
+                }
+                if (itemList.evaBonus) {
+                    items += `• EVA Bonus   : +${ itemList.evaBonus }\n`
+                }
+                if (itemList.mevaBonus) {
+                    items += `• MEVA Bonus  : +${ itemList.mevaBonus }\n`
+                }
+                if (itemList.effect) {
+                    items += `• Effect      : ${ itemList.effect }\n`
+                }
+                if (itemList.cast) {
+                    items += `• Casts on Use: ${ itemList.cast }\n`
+                }
+                if (itemList.abilities) {
+                    items += `• Abilities   : ${ itemList.abilities.join(",") }\n`
+                }
             }
-            if (itemName[0].dmgMulti) {
-                items += `• Damage Code : (${ itemList[0].dmgMulti } x ${ itemList[0].dmgAttr.toUpperCase() }) + ${ itemList[0].dmgDice }\n`;
-            }
-            if (itemList[0].armBonus) {
-                items += `• ARM Bonus   : +${ itemList[0].armBonus }\n`
-            }
-            if (itemList[0].marmBonus) {
-                items += `• MARM Bonus  : +${ itemList[0].marmBonus }\n`
-            }
-            if (itemList[0].evaBonus) {
-                items += `• EVA Bonus   : +${ itemList[0].evaBonus }\n`
-            }
-            if (itemList[0].mevaBonus) {
-                items += `• MEVA Bonus  : +${ itemList[0].mevaBonus }\n`
-            }
-            if (itemName[0].effect) {
-                items += `• Effect      : ${ itemList[0].effect }\n`
-            }
-            if (itemName[0].cast) {
-                items += `• Casts on Use: ${ itemList[0].cast }\n`
-            }
-            if (itemName[0].abilities) {
-                items += `• Abilities   : ${ itemList[0].abilities.join(",") }\n`
-            }
-        }
-        // Respond
-        message.channel.send(
+            // Respond
+            message.channel.send(
 `= ${ worldname } = World Item Details = 
 
 ${ items }`
-            ,{code: "asciidoc"});    
-    } else if (args.length = 3) {
-        // Parameters
-        let filterType = args[1], 
-            filterTier = args[2];
-        // Get builtin        
-        await client.gamedata.items.get("all").find({ type:filterType, tier:filterTier }).value().forEach((element, index) => {
-            itemList.push(element);
-        });
-        // Get custom
-        // Remove disabled
-        // Build items
-        if (itemList.length == 0) {
-            items = "Apparently, there are no items of this type and/or tier in this world, kupo...";
+                ,{code: "asciidoc"});
         } else {
-            items = "Items              | Cost  | Avail | Abilities\n" + 
-                    "----------------------------------------------\n";
-            itemList.forEach(element => {
-                items += 
-                    element.name.padEnd(20) + "| " + 
-                    element.cost.padEnd(5) + "| " +
-                    element.avail.padEnd(6) + "| " +
-                    element.abilities.join(" ") + "\n";
+            // Parameters
+            let filterTier = Number(args[1]);
+            args.shift();args.shift();
+            let filterType = args.join(" ");
+            client.logger.debug(filterType);
+            // Get builtin
+            await client.gamedata.items.get("all").filter({ type:filterType, tier:filterTier }).sortBy("avail").value().forEach((element, index) => {
+                itemList.push(element);
             });
-        }
-        // Respond
-        message.channel.send(
+            // Get custom
+            // Remove disabled
+            // Build items
+            if (itemList.length == 0) {
+                items = "Apparently, there are no items of this tier and/or type in this world, kupo...";
+            } else {
+                items = "Items               | Cost      | Avail | Abilities\n" + 
+                        "--------------------------------------------------\n";
+                itemList.forEach(element => {
+                    items += 
+                        element.name.padEnd(20) + "| " + 
+                        element.cost.toString().padStart(5) + " gil | " +
+                        element.avail.toString().padStart(4) + "% | ";
+                    if (element.abilities) {
+                        items += element.abilities.join(",");
+                    } 
+                    items += "\n";
+                });
+            }
+            // Respond
+            message.channel.send(
 `= ${ worldname } = World Tier ${ filterTier } ${ filterType } = 
 
 ${ items }
-
-Use the '/mog *worldname* item list *itemname*' command to view specific details regarding an item.`
-            ,{code: "asciidoc"});        
+Use the '/mog world *worldname* item list *itemname*' command to view specific details regarding an item.`
+                ,{code: "asciidoc"});
+        }
     } else {
-        // Get builtin        
+        // Get builtin
         await client.gamedata.items.get("all").map("type").value().forEach((element, index) => {
             if (!itemList.includes(element)) {
                 itemList.push(element);
@@ -98,15 +105,14 @@ Use the '/mog *worldname* item list *itemname*' command to view specific details
         // Remove disabled
         // Build items
         itemList.forEach(element => {
-            items += "• " + element;
+            items += `• ${element}\n`;
         });
         // Respond
         message.channel.send(
 `= ${ worldname } = World Item Types = 
 
 ${ items }
-
-Use the '/mog' *worldname* item list *type* *tier*' command to list items of specific type and tier.`
+Use the '/mog world *worldname* item list *tier* *type*' command to list items of specific tier and type.`
             ,{code: "asciidoc"});
     }
 }
