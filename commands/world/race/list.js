5ce3739253f32da89b8e91a4e682ae64320ba7af
GaiusCoffee <gaius.coffee@gmail.com>
2018-01-18 19:54:05 +0800

    Fix and enhance world.race.list

--- commands/world/race/list.js
+++ commands/world/race/list.js
@@ -5,8 +5,8 @@ exports.run = async (client, message, args, level, worldname) => {
         raceList = [], races = "";
     // Process
     // Get builtin
-    await client.gamedata.races.get("all").value().array.forEach((element, index) => {
-        raceList.push(element.race);
+    await client.gamedata.races.get("all").sortBy("race").value().forEach((element, index) => {
+        raceList.push(element);
     });
     // Get custom
     // Remove disabled
@@ -14,8 +14,13 @@ exports.run = async (client, message, args, level, worldname) => {
     if (raceList.length === 0) {
         races = "Apparently, this world has no sentient races, kupo!";
     } else {
+        races = "Race            Max | Str | Vit | Agi | Spd | Mag | Spr\n" +
+                "-------------------------------------------------------\n";
         raceList.forEach((element, index) => {
-            races += "• " + element;
+            races += `${ element.race.padEnd(20) }| ${element.attrMax.str.toString().padStart(3)} ` +
+            `| ${element.attrMax.vit.toString().padStart(3)} | ${element.attrMax.agi.toString().padStart(3)} `+
+            `| ${element.attrMax.spd.toString().padStart(3)} | ${element.attrMax.mag.toString().padStart(3)} `+
+            `| ${element.attrMax.spr.toString().padStart(3)}\n`;
         });
     }
     // Respond
